81e2f941717766cd10ca43071c55d8f1f493bb07
Everton Ferreira <evertonsilvaferreira88@gmail.com>
2024-09-23 23:06:25 -0300

    modficações main.py

--- aula01_python/main.py
+++ aula01_python/main.py
@@ -1,1 +1,20 @@
-print('Hello Wolrd')
+# print("Numero de caracteres no nome: ", len(input("Digite seu nome: ")))
+
+# a = int(input("Insira um valor: "))
+# b = int(input("Insira outro valor: "))
+
+# print("A soma destes valores é: ", a+b)
+
+CONSTANTE_BONUS = 1000
+
+nome = input('Digite seu nome: ')
+
+salario = round(float(input('Digite seu salário: ')),2)
+
+bonus = round( float(input('Digite O valor do bonus: ')),2)
+
+valor_final = CONSTANTE_BONUS + (salario * bonus)
+
+print(f'Resultado: {CONSTANTE_BONUS} +', salario * bonus )
+
+print( f'Olá {nome}, o seu valor de bônus foi de {valor_final}')
