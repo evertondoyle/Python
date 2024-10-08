97bd623387420fb4dd26a36a59586e3603b47c63
Everton Ferreira <evertonsilvaferreira88@gmail.com>
2024-10-07 22:28:07 -0300

    desafio aula 2

--- aula02_python/main.py
+++ aula02_python/main.py
@@ -1,3 +1,20 @@
-print("aula 2")
+CONSTANTE_BONUS = 1000
 
-print("aula 2 teste git")
+nome = input('Digite seu nome: ')
+
+if nome.isdigit():
+    print('Você digitou seu nome errado')
+    exit()
+elif len(nome) == 0:
+    print('Você não digitou nada!')
+    exit()
+
+salario = round(float(input('Digite seu salário: ')),2)
+
+bonus = round( float(input('Digite O valor do bonus: ')),2)
+
+valor_final = CONSTANTE_BONUS + (salario * bonus)
+
+print(f'Resultado: {CONSTANTE_BONUS} +', salario * bonus )
+
+print( f'Olá {nome}, o seu valor de bônus foi de {valor_final}')
